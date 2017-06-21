6d8f59f60462b3c7234f22d48b2b7f37c0db5108
svinjcica <zm120528d@student.etf.rs>
2017-06-21 16:29:46 +0200

    test za drugi prozor

--- test/testfirstlibrary/SecondWindJUnitTest.java
+++ test/testfirstlibrary/SecondWindJUnitTest.java
@@ -29,8 +29,18 @@ public class SecondWindJUnitTest {
     public SecondWindJUnitTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
+        @BeforeClass
+    public static void set(){
+        DataBase db = DataBase.getDB();
+        db.addBook("Na Drini cuprija", "Ivo Andric", "Zavod za udzbenike", 375, 2012);
+        db.addBook("Dervis i smrt", "Mesa Selimovic", "Stubovi kulture", 380, 2014);
+        db.addBook("Enciklopedija mrtvih", "Danilo Kis", "Stubovi kulture", 250, 2010);
+        db.addBook("Enciklopedija mrtvih 2", "Danilo Kis", "Stubovi kulture", 250, 2010);
+        
+        String s = db.createUser("maja123", "Maja", "Zivkovic",1993 );
+        s = db.createUser("sanjaz", "Sanja", "Zivkovic",1991 );
+        s = db.createUser("milena1", "Milena", "Milovanovic",1992 );
+        s = db.createUser("neko", "Pera", "Peric",1993 );
     }
     
     @AfterClass
@@ -44,8 +54,7 @@ public class SecondWindJUnitTest {
      public void testSecWind() {
       SecWin s = new SecWin();
       
-     assertNotNull(s.getUsername());
-     assertNotNull(s.getPanel()); 
+//     assertNotNull(s.getPanel()); 
      assertNotNull(s.getAutor()); 
      assertNotNull(s.getAutorField()); 
      assertNotNull(s.getIme());
@@ -57,8 +66,38 @@ public class SecondWindJUnitTest {
      }
      
     
+     @Test
+     public void testFileds() {
+      SecWin s = new SecWin();
+       
+      String name = "neko ime";
+      String author = " Ibrahim";
+      String publisher = "SomeSome";
+      
+      s.getAutorField().setText(author);
+      assertEquals(author, s.getAutorField().getText());
+      s.getImeField().setText(name);
+      assertEquals(name, s.getImeField().getText());
+      s.getIzdavacField().setText(publisher);
+      assertEquals(publisher, s.getIzdavacField().getText());
+     }
 
-
+     @Test
+     public void testTextArea(){
+         SecWin s = new SecWin();
+         assertEquals("", s.getTxt().getText());
+         
+         s.getImeField().setText("Ivo Andric");
+         assertEquals("Na Drini cuprija", s.getImeField().getText());
+         s.getAutorField().setText("");
+         assertEquals("",  s.getIzdavacField().getText());
+         s.getIzdavacField().setText("");
+         assertEquals("", s.getAutorField().getText());
+         s.getSearch().doClick();
+         
+         assertNotEquals("",s.getTxt().getText());
+         
+     }
    
 
   
