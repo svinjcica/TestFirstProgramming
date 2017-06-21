c5adb2e6e140c7015453718aaaedb004ffc1e1dd
svinjcica <zm120528d@student.etf.rs>
2017-06-21 04:10:19 +0200

    gui panel test

---
+++ test/testfirstlibrary/GuiJUnitTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testfirstlibrary;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MAJA
+ */
+public class GuiJUnitTest {
+    
+    public GuiJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void panelTest() {}
+}
---
+++ test/testfirstlibrary/PanelLoginJUnitTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testfirstlibrary;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MAJA
+ */
+public class PanelLoginJUnitTest {
+    
+    public PanelLoginJUnitTest() {
+    }
+    
+     @Test
+     public void testPanelLogin() {
+     MyPanel panel = MyPanel();
+     assertNotNull(panel.getTextLabel());
+     assertNotNull(panel.getNameLabel()); 
+     assertNotNull(panel.getNameField()); 
+     assertNotNull(panel.getPassLabel()); 
+     assertNotNull(panel.getPassField()); 
+     assertNotNull(panel.getLoginButton); 
+     
+     assertEquals("username:", panel.getNameLabel().getText());
+     assertEquals("password:", panel.getPassLabel().getText());
+     assertEquals("login", panel.getLoginButton().getText());
+     assertEquals("", panel.getPassField().getText());
+     assertEquals("", panel.getNameField().getText());
+     assertEquals("INSERT", panel.getTextLabel().getText());
+     }
+}
